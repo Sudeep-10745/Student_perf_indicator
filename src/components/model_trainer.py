246ec724a95019a8cc97757143c2a8d8d87edf13
Sudeep-10745 <sudeepwarrier4321@gmail.com>
2025-06-18 18:01:20 +0530

    Data Ingestion,Transformation and Model Training

---
+++ src/components/model_trainer.py
@@ -0,0 +1,101 @@
+#training different models and checking their accuracies
+
+#imports
+import os
+import sys
+from dataclasses import dataclass
+
+from sklearn.ensemble import (
+    AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object, evaluate_models
+
+# class for data training inputs and configuration
+@dataclass # decorator, we can directly define class variables
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting train and test input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:,:-1],  # all except the last column
+                train_array[:,-1], #last column
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+
+            #creating a dictionary of the models
+            models = {
+                "Linear Regression": LinearRegression(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "Random Forest Regressor": RandomForestRegressor(),
+                "XGBRegressor": XGBRegressor(), 
+                "AdaBoost Regressor": AdaBoostRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor()
+            }
+
+            #creating a list of parameters for hyper parameter tuning
+            #Each model will have its own set of a range of parameters 
+            params={
+                "Random Forest Regressor":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            model_report:dict = evaluate_models(X_train = X_train, y_train = y_train, X_test=X_test, y_test = y_test, models=models, params = params)
+
+            #get the best model score form the dictionary
+            best_model_score = max(sorted(model_report.values()))
+
+            #best model
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            best_model = models[best_model_name]
+
+            # if best_model_score < 0.6:
+            #     raise CustomException("No best model found")
+            logging.info(f"Best model found for both train and test data")
+
+            #save the model path
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj = best_model
+            )
+
+            predicted_best_model = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted_best_model)
+
+            return r2_square
+            
+        except Exception as e:
+            raise CustomException(e, sys)
+
+ 
